2b71450c579dc26f1a227fd112cc3927a7657858
Betelhem Tekle <111254433+bettyberry@users.noreply.github.com>
2025-03-15 01:15:37 +0300

    Update About Us.js

--- Components/About Us.js
+++ Components/About Us.js
@@ -1,4 +1,12 @@
 import React from "react";
-const About = () => <h2 className="page-content">Explore the Collection</h2>;
-export default About
 
+const About = () => {
+  return (
+    <div className="About">
+      <h2 className="About-content">About Us</h2>
+      <div><img src="/photo_2025-03-12_20-18-32.jpg" alt="About Us" /></div>
+    </div>
+  );
+};
+
+export default About;
